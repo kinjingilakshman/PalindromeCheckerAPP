8ff837abf05f888afef6d27c74744ad532d20c76
KinjingiLakshman <kinjingilakshman@gmail.com>
2026-02-23 15:36:13 +0530

    Implemented Palindrome checking logic

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,6 +1,21 @@
 public class PalindromeChecker {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application");
+    public static boolean isPalindrome(String input) {
+
+        if (input == null) return false;
+
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
---
+++ src/PalindromeCheckerAPP.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Application");
+    }
+}
