67e038cc105f81366d484e639fcb245b29789cfe
KinjingiLakshman <kinjingilakshman@gmail.com>
2026-03-10 12:42:55 +0530

    UC11: Object-Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String word) {
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        return word.equals(reversed);
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        PalindromeChecker checker = new PalindromeChecker();
+
+        String word = "madam";
+
+        boolean result = checker.checkPalindrome(word);
+
+        if (result) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
